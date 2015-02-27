4655dedc73a4a2fe3c5d1ec1b434bb658b39bf10
Liao Dianze <liaodianze@gmail.com>
2015-02-27 23:33:56 +0800

    last bugs removed

--- TextbuddyTest/unittest1.cpp
+++ TextbuddyTest/unittest1.cpp
@@ -21,7 +21,7 @@ namespace TextBuddyTests
 			Assert::AreEqual("sample", TextBuddy::getFirstWord("sample ").c_str());
 
 			Assert::AreEqual("", TextBuddy::getFirstWord("").c_str());
-			Assert::AreEqual(" ", TextBuddy::getFirstWord(" ").c_str());
+			Assert::AreEqual("", TextBuddy::getFirstWord(" ").c_str());
 
 		}
 
@@ -83,84 +83,83 @@ namespace TextBuddyTests
 		}
 
 		TEST_METHOD(clearAllTest) {
-			const string ExpOutput = "All message is deleted from heytemp123.txt";
+			const string ExpOutput = "All message is deleted from fileName.txt";
 
-			Assert::AreEqual(ExpOutput, TextBuddy::clearAll("heytemp123.txt"));
+			Assert::AreEqual(ExpOutput, TextBuddy::clearAll("fileName.txt"));
 		}
 
 		TEST_METHOD(addLineTest) {
 
 			const string addInput = "hello there!";
-			const string ExpOutput = "Added to heytemp123.txt: \"hello there!\"";
+			const string ExpOutput = "Added to fileName.txt: \"hello there!\"";
 
-			Assert::AreEqual(ExpOutput, TextBuddy::addLine("heytemp123.txt", addInput));
+			Assert::AreEqual(ExpOutput, TextBuddy::addLine("fileName.txt", addInput));
 		}
 
 
 		TEST_METHOD(displayAllTest) {
 
-			const string ExpEmptyOutput = "heytemp123.txt is empty";
+			const string ExpEmptyOutput = "fileName.txt is empty";
 			const string ExpOutput = "";
 
 			//clear file
-			TextBuddy::clearAll("heytemp123.txt");
-			TextBuddy::writeToFile("heytemp123.txt");
+			TextBuddy::clearAll("fileName.txt");
+			TextBuddy::writeToFile("fileName.txt");
 
-			Assert::AreEqual(ExpEmptyOutput, TextBuddy::displayAll("heytemp123.txt"));
+			Assert::AreEqual(ExpEmptyOutput, TextBuddy::displayAll("fileName.txt"));
 
 			//set up file
-			TextBuddy::addLine("heytemp123.txt", "sample text");
-			TextBuddy::addLine("heytemp123.txt", "sample ");
-			TextBuddy::writeToFile("heytemp123.txt");
+			TextBuddy::addLine("fileName.txt", "sample text");
+			TextBuddy::addLine("fileName.txt", "sample ");
+			TextBuddy::writeToFile("fileName.txt");
 
-			Assert::AreEqual(ExpOutput, TextBuddy::displayAll("heytemp123.txt"));
+			Assert::AreEqual(ExpOutput, TextBuddy::displayAll("fileName.txt"));
 
 			//clear file
-			TextBuddy::clearAll("heytemp123.txt");
-			TextBuddy::writeToFile("heytemp123.txt");
+			TextBuddy::clearAll("fileName.txt");
+			TextBuddy::writeToFile("fileName.txt");
 		}
 
 		TEST_METHOD(deleteLineTest) {
-			const string ExpOutput = "Deleted from heytemp123.txt: \"sample \"";
+			const string ExpOutput = "Deleted from fileName.txt: \"sample \"";
 
 			//set up file
-			TextBuddy::addLine("heytemp123.txt", "sample text");
-			TextBuddy::addLine("heytemp123.txt", "sample ");
-			TextBuddy::writeToFile("heytemp123.txt");
+			TextBuddy::addLine("fileName.txt", "sample text");
+			TextBuddy::addLine("fileName.txt", "sample ");
+			TextBuddy::writeToFile("fileName.txt");
 
-			Assert::AreEqual(ExpOutput, TextBuddy::deleteLine("heytemp123.txt", "2"));
+			Assert::AreEqual(ExpOutput, TextBuddy::deleteLine("fileName.txt", "2"));
 
 			//clear file
-			TextBuddy::clearAll("heytemp123.txt");
-			TextBuddy::writeToFile("heytemp123.txt");
+			TextBuddy::clearAll("fileName.txt");
+			TextBuddy::writeToFile("fileName.txt");
 		}
 
 		TEST_METHOD(sortAlphaTest) {
 
-			const string ExpOutput = "All messages in heytemp123.txt are sorted alphabetically";
+			const string ExpOutput = "all messages in fileName.txt are sorted alphabetically";
 
-			Assert::AreEqual(ExpOutput, TextBuddy::sortLineAlphabetically("heytemp123.txt"));
+			Assert::AreEqual(ExpOutput, TextBuddy::sortLineAlphabetically("fileName.txt"));
 
 		}
 
 		TEST_METHOD(searchForWordTest) {
-			//const string ExpOutput1 = "Message is found 2 in heytemp123.txt";
-			//const string ExpOutput2 = "Message is found 1 in heytemp123.txt";
-			const string ExpOutput3 = "message cannot be found in heytemp123.txt";
+
+			const string ExpOutput2 = "message is found at 1 in fileName.txt";
+			const string ExpOutput3 = "message cannot be found in fileName.txt";
 
 			//set up file
-			TextBuddy::addLine("heytemp123.txt", "sample text");
-			TextBuddy::addLine("heytemp123.txt", "I am right");
-			TextBuddy::writeToFile("heytemp123.txt");
+			TextBuddy::addLine("fileName.txt", "sample text");
+			TextBuddy::addLine("fileName.txt", "I am right");
+			TextBuddy::writeToFile("fileName.txt");
 
-			//Assert::AreEqual(ExpOutput1, TextBuddy::searchForWord("heytemp123.txt", "sample"));
-			//Assert::AreEqual(ExpOutput2, TextBuddy::searchForWord("heytemp123.txt", "sample text"));
-			//Assert::AreEqual(ExpOutput2, TextBuddy::searchForWord("heytemp123.txt", "text"));
-			Assert::AreEqual(ExpOutput3, TextBuddy::searchForWord("heytemp123.txt", "hello"));
+			Assert::AreEqual(ExpOutput2, TextBuddy::searchForWord("fileName.txt", "sample text"));
+			Assert::AreEqual(ExpOutput2, TextBuddy::searchForWord("fileName.txt", "text"));
+			Assert::AreEqual(ExpOutput3, TextBuddy::searchForWord("fileName.txt", "hello"));
 
 			//clear file
-			TextBuddy::clearAll("heytemp123.txt");
-			TextBuddy::writeToFile("heytemp123.txt");
+			TextBuddy::clearAll("fileName.txt");
+			TextBuddy::writeToFile("fileName.txt");
 		}
 	};
 }
