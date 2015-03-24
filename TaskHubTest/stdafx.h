6f8997b7dc12d7d3be982ba821736384648d145f
Xuan <chensx245@gmail.com>
2015-03-24 14:05:12 +0800

    Set up unit test project

---
+++ TaskHubTest/stdafx.h
@@ -0,0 +1,28 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+// Headers for CppUnitTest
+#include "CppUnitTest.h"
+
+// TODO: reference additional headers your program requires here
+#include "..\TaskHub\CommandAdd.h"
+#include "..\TaskHub\CommandDelete.h"
+#include "..\TaskHub\CommandDisplay.h"
+#include "..\TaskHub\CommandInterpreter.h"
+#include "..\TaskHub\CommandMarkDone.h"
+#include "..\TaskHub\CommandRedo.h"
+#include "..\TaskHub\CommandSearch.h"
+#include "..\TaskHub\CommandUndo.h"
+#include "..\TaskHub\CommandUpdate.h"
+#include "..\TaskHub\DateParser.h"
+#include "..\TaskHub\History.h"
+#include "..\TaskHub\Logic.h"
+#include "..\TaskHub\Parser.h"
+#include "..\TaskHub\StorageController.h"
+#include "..\TaskHub\Ui.h"
