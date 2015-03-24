ae17915dd114dbd8f8adba37bae415e5f4725a54
Xuan <chensx245@gmail.com>
2015-03-24 16:35:42 +0800

    update test cpp file name

---
+++ TaskHubTest/TaskHubTest.cpp
@@ -0,0 +1,41 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TaskHubTest
+{		
+	TEST_CLASS(UITest){
+	public:
+		
+		TEST_METHOD(UITestMethod1){
+			// TODO: Your test code here
+		}
+
+	};
+
+	TEST_CLASS(LogicTest){
+    public:
+
+		TEST_METHOD(LogicTestMethod1){
+
+		}
+	};
+
+	TEST_CLASS(ParserTest){
+	public:
+
+		TEST_METHOD(ParserTestMethod1){
+
+		}
+	};
+
+	TEST_CLASS(StorageTest){
+	public:
+
+		TEST_METHOD(StorageTestMethod1){
+
+		}
+
+	};
+}
