00b9558a6631d9496fc27619fc4c3561223e4de5
Wang Bowen <bowen_wang@hotmail.com>
2015-03-27 00:28:03 +0800

    Revert "parser code for DateParser, flexible date"
    This reverts commit 65437f5fde831bc4529d05dcd998a0ed4db0d139.

--- TaskHub/DateParser.cpp
+++ TaskHub/DateParser.cpp
@@ -7,29 +7,7 @@ DateParser::DateParser(void){
 
 DateParser::DateParser(string input){
 	size_t get_date = input.find("/");
-	if(input.substr(get_date-2,0) != " "){
-		if(input.substr(get_date+2,0) != " "){
-			_date = input.substr(get_date-2, 5);
-		}
-		else{
-			if(input.substr(get_date+2,0) == " "){
-				_date = input.substr(get_date-2, 4);
-			}
-		}
-		
-	}
-	else{
-		if(input.substr(get_date-2,0) == " "){
-			if(input.substr(get_date+2, 0) != " "){
-				_date = input.substr(get_date-1, 4);
-			}
-			else{
-				if(input.substr(get_date+2, 0) == " "){
-					_date = input.substr(get_date-1, 3);
-				}
-			}
-		}
-	}
+	_date = input.substr(get_date - 2, 5);
 }
 
 string DateParser::getDate(){
