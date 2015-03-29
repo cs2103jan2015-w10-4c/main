0085f15c2716ad3995999a867be90f2f7cb0eb26
Xuan <chensx245@gmail.com>
2015-03-29 23:17:35 +0800

    Separate unit test cpp according to test components

--- TaskHubTest/LogicTest.cpp
+++ TaskHubTest/LogicTest.cpp
@@ -3,16 +3,7 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace TaskHubTest
-{		
-	TEST_CLASS(UITest){
-	public:
-		
-		TEST_METHOD(UITestMethod1){
-			// TODO: Your test code here
-		}
-
-	};
+namespace LogicTest{		
 
 	TEST_CLASS(LogicTest){
     public:
@@ -54,24 +45,4 @@ namespace TaskHubTest
 		}
 	};
 
-	TEST_CLASS(ParserTest){
-	public:
-		//test on funtionality of VenueParser class
-		TEST_METHOD(TestVenueParser){
-			std::string testInput = "Write code @NUS";
-			VenueParser parseVenue(testInput);
-			std::string expectedOutput = "NUS";
-			std::string realOutput = parseVenue.getVenue();
-			Assert::AreEqual(realOutput, expectedOutput);
-		}
-	};
-
-	TEST_CLASS(StorageTest){
-	public:
-
-		TEST_METHOD(StorageTestMethod1){
-
-		}
-
-	};
 }
