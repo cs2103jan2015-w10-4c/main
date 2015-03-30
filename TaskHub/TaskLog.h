3705a45a50ce4e7a428240d8911af5fe1f16a3bd
Dianze <liaodianze@gmail.com>
2015-03-30 19:50:23 +0800

    Create taskLog Class Removal of task sub classes

---
+++ TaskHub/TaskLog.h
@@ -0,0 +1,8 @@
+#pragma once
+class TaskLog
+{
+public:
+	TaskLog(void);
+	~TaskLog(void);
+};
+
--- TaskHub/TaskRecurring.cpp
+++
@@ -1,9 +0,0 @@
-#include "TaskRecurring.h"
-
-
-TaskRecurring::TaskRecurring(){
-}
-
-
-TaskRecurring::~TaskRecurring(){
-}
