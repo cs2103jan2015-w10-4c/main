bb179f10465677e9d12ba035a418dc39e9323c8e
Wang Bowen <bowen_wang@hotmail.com>
2015-04-01 20:38:08 +0800

    weird messages removed

--- TaskHub/TimeParser.cpp
+++ TaskHub/TimeParser.cpp
@@ -9,15 +9,11 @@ TimeParser::TimeParser(string input){
 	size_t ending_time = input.find("-to");
 	_startTime = input.substr(timed_Task + 6, 5);
 	_endTime = input.substr(ending_time + 4, 5);
-<<<<<<< HEAD
+
 	_startHour = atoi(input.substr(timed_Task+6, 2).c_str());
 	_startMinute = atoi(input.substr(timed_Task+9, 2).c_str());
 	_endHour = atoi(input.substr(ending_time+4, 2).c_str());
 	_endMinute = atoi(input.substr(ending_time+7, 2).c_str());
-=======
-	_hour = atoi(input.substr(timed_Task+6, 2).c_str());
-	_minute = atoi(input.substr(timed_Task+9, 2).c_str());
->>>>>>> 406445856a424c95f96b866652307badb6ccb5b3
 }
 
 string TimeParser::getStartTime(){
@@ -28,7 +24,7 @@ string TimeParser::getEndTime(){
 	return _endTime;
 }
 
-<<<<<<< HEAD
+
 int TimeParser::getStartHour(){
 	return _startHour;
 }
@@ -43,12 +39,4 @@ int TimeParser::getEndHour(){
 
 int TimeParser::getEndMinute(){
 	return _endMinute;
-=======
-int TimeParser::getHour(){
-	return _hour;
-}
-
-int TimeParser::getMinute(){
-	return _minute;
->>>>>>> 406445856a424c95f96b866652307badb6ccb5b3
 }
--- TaskHub/TimeParser.h
+++ TaskHub/TimeParser.h
@@ -10,7 +10,6 @@ public:
 	TimeParser(string input);
 	string getStartTime();
 	string getEndTime();
-<<<<<<< HEAD
 	int getStartHour();
 	int getStartMinute();
 	int getEndHour();
@@ -22,14 +21,5 @@ private:
 	int _startMinute;
 	int _endHour;
 	int _endMinute;
-=======
-	int getHour();
-	int getMinute();
-private:
-	string _startTime;
-	string _endTime;
-	int _hour;
-	int _minute;
->>>>>>> 406445856a424c95f96b866652307badb6ccb5b3
 };
 #endif
