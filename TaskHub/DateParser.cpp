ff397a9aba025a5d6ff180c22ccb645f3481d061
Wang Bowen <bowen_wang@hotmail.com>
2015-04-02 11:48:01 +0800

    bug fix of date parser

--- TaskHub/DateParser.cpp
+++ TaskHub/DateParser.cpp
@@ -7,16 +7,16 @@ DateParser::DateParser(void){
 
 DateParser::DateParser(string input){
 	size_t get_date = input.find("/");
-	if(input.substr(get_date-2,0) != " "){
+	if(input.substr(get_date-2,1) != " "){
 		//  format:  dd/mm
-		if(input.substr(get_date+2,0) != " "){
+		if(input.substr(get_date+2,1) != " "){
 			_date = input.substr(get_date-2, 5);
 			_day = atoi(input.substr(get_date-2,2).c_str());
 			_month = atoi(input.substr(get_date+1, 2).c_str());
 		}
 		// format:  dd/m
 		else{
-			if(input.substr(get_date+2,0) == " "){
+			if(input.substr(get_date+2,1) == " "){
 				_date = input.substr(get_date-2, 4);
 				_day = atoi(input.substr(get_date-2,2).c_str());
 				_month = atoi(input.substr(get_date+1, 1).c_str());
@@ -25,16 +25,16 @@ DateParser::DateParser(string input){
 		
 	}
 	else{
-		if(input.substr(get_date-2,0) == " "){
+		if(input.substr(get_date-2,1) == " "){
 			// format: d/mm
-			if(input.substr(get_date+2, 0) != " "){
+			if(input.substr(get_date+2, 1) != " "){
 				_date = input.substr(get_date-1, 4);
 				_day = atoi(input.substr(get_date-1, 1).c_str());
 				_month = atoi(input.substr(get_date+1, 2).c_str());
 			}
 			// format: d/m
 			else{
-				if(input.substr(get_date+2, 0) == " "){
+				if(input.substr(get_date+2, 1) == " "){
 					_date = input.substr(get_date-1, 3);
 					_day = atoi(input.substr(get_date-1, 1).c_str());
 					_month = atoi(input.substr(get_date+1, 1).c_str());
