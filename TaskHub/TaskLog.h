40850e9136ea4555bca2889f4c60c3a2a3bfa42f
Xuan <chensx245@gmail.com>
2015-04-04 17:48:44 +0800

    Define TaskLog class

--- TaskHub/TaskLog.h
+++ TaskHub/TaskLog.h
@@ -1,8 +1,35 @@
 #pragma once
-class TaskLog
-{
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class TaskLog{
+
 public:
-	TaskLog(void);
-	~TaskLog(void);
+	TaskLog();
+	~TaskLog();
+	
+	void updateTaskLog(std::string input);
+	void printTaskLog();
+	void clearTaskLog();
+
+	void readLogFile();
+	void updateLogFile();
+	void printClearedTaskLogMessage();
+
+	std::string removeIndexFromString(std::string logString);
+	std::string getTaskLog();
+	std::string getLogFileName();
+
+private:
+	char buffer[255];
+	std::string _logFileName;
+	std::string MESSAGE_TASKLOG_CLEARED;
+	std::vector<std::string> tempLogStorage;
+
 };
+
 
