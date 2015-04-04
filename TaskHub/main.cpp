3983d756235d2488d1c6a3c2364ed47309774302
shuyuan <lishuyuan133@hotmail.com>
2015-04-04 17:09:55 +0800

    main function edited

--- TaskHub/main.cpp
+++ TaskHub/main.cpp
@@ -1,17 +1,36 @@
 #include "UI.h"
 #include "CommandInterpreter.h"
+#include <stdlib.h>
 
 int main() {
-	UI::showToUser(UI::displayWelcomeMessage());
-	cout << "=========================================="<<endl;
+	/*
 	while (true) {
 
+		//system("CLS");
+		UI::showToUser("xx",UI::displayWelcomeMessage());
+		cout << "=========================================="<<endl;
 		string userCommand = UI::getUserCommand();
 		string output = CommandInterpreter::executeUserCommand(userCommand);
-		UI::showToUser(output);
+		UI::showToUser(userCommand,output);
 		cout << "\n===================================="<<endl;;
     }
 
 //	system("pause");
+*/
+		cout<< UI::displayWelcomeMessage()<< endl;
+		cout << "=========================================="<<endl;
+		cout << ShowDailyTask::showDayTask("today")<<endl;
+		string userCommand = UI::getUserCommand();
+		system("CLS");
+		while(!userCommand.empty()){
+
+			string output = CommandInterpreter::executeUserCommand(userCommand);
+			UI::showToUser(userCommand,output);
+			cout << "\n===================================="<<endl;;
+			userCommand = UI::getUserCommand();
+			system("CLS");
+			cout<< UI::displayWelcomeMessage() << endl;
+			cout << "=========================================="<<endl;
+		}
 	return 0;
 }
