0c4c77fa52b8f6b3c18fa3d42e382953b91d55d9
Xuan <chensx245@gmail.com>
2015-04-04 17:51:28 +0800

    Refine storageController class

--- TaskHub/StorageController.cpp
+++ TaskHub/StorageController.cpp
@@ -1,33 +1,90 @@
 #include "StorageController.h"
 
-vector<string> StorageController::Task;
-const string StorageController::fileName = "storage.txt";
+vector<string> StorageController::TaskList;
+std::string StorageController::_fileName;
+TaskLog* StorageController::taskLog;
 
+StorageController::StorageController(){
+}
+
+StorageController::~StorageController(){
+}
+
+//change function name to updateSaveFile
 void StorageController::programmeTerminating() {
 	ofstream file;
-	string currentLine;
 
-	remove(fileName.c_str());
-
-	file.open(fileName);
-	currentLine = Logic::display("all");
-	file << currentLine << endl;	
+	file.open(getFileName());
+	file << convertTaskIntoString();	
 	file.close();
 }
 
-void StorageController::programmeInitialising() {
+std::string StorageController::convertTaskIntoString(){
+	ostringstream oss;
+	vector<Task> temp = Logic::history.getVectorTextStorage();
+
+	if (temp.empty()){
+		return "";
+	}
+	else{
+		std::vector<Task>::iterator iter;
+		int taskIndex = 1;
+		
+		for (iter = temp.begin(); iter != temp.end(); iter++){
+			oss << taskIndex << ". " << iter->ToString();
+			if (iter != temp.end() - 1){
+				oss << std::endl;
+				taskIndex++;
+			}
+		}
+		return oss.str();
+	}
+}
+
+void StorageController::programmeInitialising(){
+	constructTaskLog();
+	promptSaveFile();
+	readSaveFile();
+}
+
+void StorageController::promptSaveFile(){
+	std::cout << "Enter save file address: ";
+	std::string fileName;
+	std::cin >> fileName;
+	setFileName(fileName);
+}
+
+void StorageController::constructTaskLog(){
+	taskLog = new TaskLog();
+}
+
+void StorageController::destructTaskLog(){
+	taskLog->~TaskLog();
+}
+
+void StorageController::readSaveFile() {
 	ifstream file;
 	string currentLine;
 
-	Task.clear();
-	file.open(fileName);
+	TaskList.clear();
+
+	file.open(getFileName());
 
 	while(getline(file,currentLine)) {
-		Task.push_back(currentLine);
+		TaskList.push_back(currentLine);
 	}
 	file.close();
 }
 
-vector<string> StorageController::returnTask() {
-	return Task;
+void StorageController::setFileName(std::string fileName){
+	_fileName = fileName;
 }
+
+std::string StorageController::getFileName(){
+	return _fileName;
+}
+
+vector<string> StorageController::returnTask() {
+	return TaskList;
+}
+
