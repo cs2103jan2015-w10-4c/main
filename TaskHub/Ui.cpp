f6830a11fb2a81601af00ff6c72cbe7e02de70fc
shuyuan <lishuyuan133@hotmail.com>
2015-04-04 17:09:05 +0800

    UI class edited

--- TaskHub/Ui.cpp
+++ TaskHub/Ui.cpp
@@ -10,7 +10,22 @@ string UI::displayWelcomeMessage() {
 	return MESSAGE_WELCOME;
 }
 
-void UI::showToUser(string text) {
+void UI::showToUser(string userCommand, string text) {
+	size_t _command = userCommand.find_first_of(" ");
+	string command = userCommand.substr(0,_command);
+	string message = userCommand.substr(_command+1);
+	
+	if(command=="show"){
+		if(message=="today"){
+			cout << ShowDailyTask::showDayTask("today") << endl<<endl<<endl<<endl;
+		}
+		else{
+			cout << ShowDailyTask::showDayTask(message) << endl<<endl<<endl<<endl;
+		}
+	}
+	else{
+		cout << ShowDailyTask::showDayTask("today") << endl<<endl<<endl<<endl;
+	}
 	cout << text << endl;
 }
 
