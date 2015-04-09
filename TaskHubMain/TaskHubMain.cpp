07976012ca3ecf95756ce288989f307868469b8b
shuyuan <lishuyuan133@hotmail.com>
2015-04-09 20:40:16 +0800

    taskhub main edited

--- TaskHubMain/TaskHubMain.cpp
+++ TaskHubMain/TaskHubMain.cpp
@@ -1,7 +1,37 @@
 #include "..\TaskHub\UI.h"
 #include "..\TaskHub\CommandInterpreter.h"
 
+//#include "UI.h"
+//#include "CommandInterpreter.h"
+#include <stdlib.h>
+#include <Windows.h>
+
 int main() {
+	
+
+	UI::programmeInitialising();
+	UI::displayWelcomeMessage();
+//cout << "\n============================================================================" << endl;
+	UI::showToUser("show today");
+	while (true) {
+
+		
+		string userCommand = UI::getUserCommand();
+		cout << endl;
+		string output = CommandInterpreter::executeUserCommand(userCommand);
+		UI::displayWelcomeMessage();
+		UI::showToUser(userCommand);
+		cout << endl<< endl<< endl;
+		UI::systemFeedback(output);
+		cout << "\n=============================================================================="<<endl;;
+	}
+
+	return 0;
+}
+
+
+
+/*int main() {
 	UI::showToUser(UI::displayWelcomeMessage());
 	cout << "==========================================" << endl;
 	while (true) {
@@ -15,4 +45,4 @@ int main() {
 
 	//	system("pause");
 	return 0;
-}
+}*/
