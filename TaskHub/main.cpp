ac14b62cb551261980ee42b91cbcd17b1459bb62
shuyuan <lishuyuan133@hotmail.com>
2015-04-09 20:38:10 +0800

    main updated

--- TaskHub/main.cpp
+++ TaskHub/main.cpp
@@ -1,36 +1,27 @@
 #include "UI.h"
 #include "CommandInterpreter.h"
 #include <stdlib.h>
+#include <Windows.h>
 
 int main() {
-	/*while (true) {
+	
 
-		//system("CLS");
-		UI::showToUser(UI::displayWelcomeMessage());
-		cout << "=========================================="<<endl;
+	UI::programmeInitialising();
+	UI::displayWelcomeMessage();
+//cout << "\n============================================================================" << endl;
+	UI::showToUser("show today");
+	while (true) {
+
+		
 		string userCommand = UI::getUserCommand();
+		cout << endl;
 		string output = CommandInterpreter::executeUserCommand(userCommand);
+		UI::displayWelcomeMessage();
 		UI::showToUser(userCommand);
-		cout << "\n===================================="<<endl;;
-    }
+		cout << endl<< endl<< endl;
+		UI::systemFeedback(output);
+		cout << "\n=============================================================================="<<endl;;
+	}
 
-//	system("pause"); */
-
-		cout<< UI::displayWelcomeMessage()<< endl;
-		cout << "=========================================="<<endl;
-		cout << ShowDailyTask::showDayTask("today")<<endl;
-		string userCommand = UI::getUserCommand();
-		system("CLS");
-		while(!userCommand.empty()){
-
-			string output = CommandInterpreter::executeUserCommand(userCommand);
-			UI::showToUser(userCommand);
-			UI::systemFeedback(output);
-			cout << "\n===================================="<<endl;;
-			userCommand = UI::getUserCommand();
-			system("CLS");
-			//cout<< UI::displayWelcomeMessage() << endl;
-			cout << "=========================================="<<endl;
-		}
 	return 0;
 }
