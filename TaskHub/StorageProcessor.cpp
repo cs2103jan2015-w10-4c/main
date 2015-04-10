47584d0658dd6b4774e861deabff9695b9c7e255
Xuan <chensx245@gmail.com>
2015-04-10 14:23:41 +0800

    refactor convertTaskIntoString from StorageController to StorageProcessor

--- TaskHub/StorageProcessor.cpp
+++ TaskHub/StorageProcessor.cpp
@@ -7,3 +7,25 @@ StorageProcessor::StorageProcessor(){
 
 StorageProcessor::~StorageProcessor(){
 }
+
+std::string StorageProcessor::convertTaskIntoString(){
+	std::ostringstream oss;
+	std::vector<Task> temp = Logic::history.getVectorTextStorage();
+
+	if (temp.empty()){
+		return "";
+	}
+	else{
+		std::vector<Task>::iterator iter;
+		int taskIndex = 1;
+
+		for (iter = temp.begin(); iter != temp.end(); iter++){
+			oss << taskIndex << ". " << iter->ToString();
+			if (iter != temp.end() - 1){
+				oss << std::endl;
+				taskIndex++;
+			}
+		}
+		return oss.str();
+	}
+}
