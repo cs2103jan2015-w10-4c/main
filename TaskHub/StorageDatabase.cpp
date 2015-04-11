420aad5c211f259992523e31ec990ea7403d351d
Xuan <chensx245@gmail.com>
2015-04-11 10:53:41 +0800

    introduce object pointers in storageController

--- TaskHub/StorageDatabase.cpp
+++ TaskHub/StorageDatabase.cpp
@@ -7,3 +7,11 @@ StorageDatabase::StorageDatabase(){
 
 StorageDatabase::~StorageDatabase(){
 }
+
+
+void StorageDatabase::executeUpdateSaveFile(std::string filename, std::string inputString){
+	std::ofstream file;
+	file.open(filename);
+	file << inputString;
+	file.close();
+}
