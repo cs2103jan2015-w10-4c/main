f7fd36d3a59d7faf8dbc19e8f1a6e39b4acf2131
Xuan <chensx245@gmail.com>
2015-04-13 02:30:19 +0800

    add unit tests

--- TaskHubTest/StorageControllerTest.cpp
+++ TaskHubTest/StorageControllerTest.cpp
@@ -1,3 +1,5 @@
+//@author A0111322E
+
 #include "stdafx.h"
 #include "CppUnitTest.h"
 
--- TaskHubTest/StorageProcessorTest.cpp
+++ TaskHubTest/StorageProcessorTest.cpp
@@ -1,3 +1,5 @@
+//@author A0111322E
+
 #include "stdafx.h"
 #include "CppUnitTest.h"
 
@@ -9,8 +11,22 @@ namespace TaskHubTest{
 
 	public:
 		
-		TEST_METHOD(TestStorageProcessor){
-			// TODO: Your test code here
+		TEST_METHOD(TestProcessFileDirectory){
+			StorageProcessor* test = new StorageProcessor();
+
+			//test case 1	w/o quotation marks
+			std::string testResult1 = test->processFileDirectory("abc.txt");
+			std::string expectedResult1("abc.txt");
+			Assert::AreEqual(testResult1, expectedResult1);
+
+			//test case 2	with quotation marks
+			std::string testString("\"abc.txt\"");
+			std::string testResult2 = test->processFileDirectory("\"abc.txt\"");
+			Assert::AreEqual(testResult2, expectedResult1);
+			Assert::AreEqual(testResult1, testResult2);
+			Assert::AreNotEqual(testString, testResult2);
+
+			delete test;
 		}
 
 	};
